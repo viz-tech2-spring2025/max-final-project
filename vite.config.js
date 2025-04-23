c351eb9dc7555017b029ad6a7bac1b436350ed11
Max <max.spencer@xtra.co.nz>
2025-04-23 13:19:42 -0400

    removed deployment changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/max-final-project",
   plugins: [react()],
   optimizeDeps: {
     include: ['@deck.gl/mapbox']
